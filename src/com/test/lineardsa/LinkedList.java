ba66c059e62852730dd65d3d7d134ee49fc86445
Satish M Hiremath <satishmhiremath@gmail.com>
2024-12-20 00:32:53 +0530

    Update LinkedList.java

--- src/com/test/lineardsa/LinkedList.java
+++ src/com/test/lineardsa/LinkedList.java
@@ -40,6 +40,22 @@ public class LinkedList {
         linkedList.printList();
     }
 
+    public ListNode oddEvenList(ListNode head) {
+         if(head == null) {
+             return null;
+         }   
+        Node odd = head;
+        Node even = head.next;
+        Node evenHead = even;
+        while(even != null && even.next != null) {
+            odd.next=even.next;
+            odd=odd.next;
+            even.next=odd.next;
+            even=even.next;
+        }
+        odd.next = evenHead;
+        return head
+    }
     private void printList() {
         Node curr = head;
         System.out.println("Linked List : ");

4fff5f4ef9694fd50d441d755b6f09ede6b6df63
Satish M Hiremath <satishmhiremath@gmail.com>
2024-12-19 08:16:34 +0530

    Update LinkedList.java

--- src/com/test/lineardsa/LinkedList.java
+++ src/com/test/lineardsa/LinkedList.java
@@ -2,96 +2,157 @@ package com.test.lineardsa;
 
 public class LinkedList {
 
-	private Node head;
+    private Node head;
 
-	class Node {
-		private int data;
-		private Node next;
-		public Node(int val) {
-			this.data = val;
-			this.next = null;
-		}
-	}
+    class Node {
+        private int data;
+        private Node next;
+        public Node(int val) {
+            this.data = val;
+            this.next = null;
+        }
+    }
 
-	public static void main(String[] args) {
-		LinkedList linkedList = new LinkedList();
-		linkedList.addNode(10);
-		linkedList.addNode(20);
-		linkedList.addNode(30);
-		linkedList.addNode(40);
-		//		linkedList.removeNode(20);
-		//		linkedList.insertAtPosition(10);
-		//		linkedList.insertAtBegining(100);
-		//		linkedList.insertAtEnd(200);
-		System.out.println();
-		linkedList.printList();
-		linkedList.reverseNode();
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.addNode(10);
+        linkedList.addNode(20);
+        linkedList.addNode(30);
+        linkedList.addNode(40);
+        linkedList.printList();
 
-	}
+        linkedList.removeNode(20);
+        linkedList.printList();
 
-	private void printList() {
-		Node curr = head;
-		System.out.println("Linked List : ");
-		while(curr != null ) {
-			System.out.print(curr.data);
-			if(curr.next != null) {
-				System.out.print(" -> ");
-			}
-			curr=curr.next;
-		}
-		System.out.println();
-	}
+        linkedList.removeNodeAtPosition(2);
+        linkedList.printList();
 
-	private void addNode(int val) {
-		Node newNode = new Node(val);
-		Node curr = null;
-		if(head == null) {
-			head = newNode;
-		}else {
-			curr = head;
-			while(curr.next != null) {	
-				curr = curr.next;
-			}
-			curr.next = newNode;
-		}
-	}
+        linkedList.insertAtPosition(25, 2);
+        linkedList.printList();
 
-	private void removeNode(int val) {
+        linkedList.insertAtBeginning(100);
+        linkedList.printList();
 
-	}
+        linkedList.insertAtEnd(200);
+        linkedList.printList();
 
-	private void insertAtPosition(int val) {
+        linkedList.reverseNode();
+        linkedList.printList();
+    }
 
-	}
+    private void printList() {
+        Node curr = head;
+        System.out.println("Linked List : ");
+        while (curr != null) {
+            System.out.print(curr.data);
+            if (curr.next != null) {
+                System.out.print(" -> ");
+            }
+            curr = curr.next;
+        }
+        System.out.println();
+    }
 
-	private void insertAtBegining(int val) {
+    private void addNode(int val) {
+        Node newNode = new Node(val);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node curr = head;
+            while (curr.next != null) {
+                curr = curr.next;
+            }
+            curr.next = newNode;
+        }
+    }
 
-	}
+    private void removeNode(int val) {
+        if (head == null) return;
 
-	private void insertAtEnd(int val) {
+        if (head.data == val) {
+            head = head.next;
+            return;
+        }
 
-	} 
+        Node curr = head;
+        while (curr.next != null && curr.next.data != val) {
+            curr = curr.next;
+        }
 
-	private void reverseNode() {
-		Node curr = head;
-		Node prev = null;
-		Node next = null;
-		
-		if(head == null) {
-			System.out.println("Node head is null");
-		}
-//		System.out.print(curr.data+" -> ");
-		while(curr != null && curr.next != null) {
-			next = curr.next;
-			curr.next = prev;
-			prev = curr;
-			curr = next;
-			System.out.print(prev.data+" ");
-			if(curr.next != null) {
-				System.out.print(" -> ");
-			}
-		}
-		head=prev;
-	}
+        if (curr.next != null) {
+            curr.next = curr.next.next;
+        }
+    }
 
+    private void removeNodeAtPosition(int position) {
+        if (head == null || position < 0) return;
+
+        if (position == 0) {
+            head = head.next;
+            return;
+        }
+
+        Node curr = head;
+        for (int i = 0; curr != null && i < position - 1; i++) {
+            curr = curr.next;
+        }
+
+        if (curr == null || curr.next == null) return;
+
+        curr.next = curr.next.next;
+    }
+
+    private void insertAtPosition(int val, int position) {
+        Node newNode = new Node(val);
+
+        if (position == 0) {
+            newNode.next = head;
+            head = newNode;
+            return;
+        }
+
+        Node curr = head;
+        for (int i = 0; curr != null && i < position - 1; i++) {
+            curr = curr.next;
+        }
+
+        if (curr == null) return;
+
+        newNode.next = curr.next;
+        curr.next = newNode;
+    }
+
+    private void insertAtBeginning(int val) {
+        Node newNode = new Node(val);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    private void insertAtEnd(int val) {
+        Node newNode = new Node(val);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node curr = head;
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+        curr.next = newNode;
+    }
+
+    private void reverseNode() {
+        Node curr = head;
+        Node prev = null;
+        Node next = null;
+        
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
 }
